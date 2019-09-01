7a3430f9cf84e498e844b472460cb6f1c871b0b2
Frank Wang <fwcontact@me.com>
2019-09-02 01:39:39 +0800

    topic exchange

--- src/main/java/com/example/demo/MyBean.java
+++ src/main/java/com/example/demo/MyBean.java
@@ -6,9 +6,9 @@ import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -29,24 +29,60 @@ public class MyBean {
 		this.log.info(this.amqpTemplate);
 
 		// declare queue
-		final Queue queue = new Queue("dev");
-		this.amqpAdmin.declareQueue(queue);
+		final Queue queue1 = new Queue("queue1");
+		this.amqpAdmin.declareQueue(queue1);
+
+		final Queue queue2 = new Queue("queue2");
+		this.amqpAdmin.declareQueue(queue2);
+
+		// declare exchange
+		final TopicExchange exchange = (TopicExchange) ExchangeBuilder
+				.topicExchange("amq.topic")
+				.build();
 
 		// declare binding
-		final DirectExchange exchange = (DirectExchange) ExchangeBuilder
-				.directExchange("amq.direct")
-				.build();
-		final Binding binding = BindingBuilder
-				.bind(queue)
+		final Binding binding1 = BindingBuilder
+				.bind(queue1)
 				.to(exchange)
-				.withQueueName();
-		this.amqpAdmin.declareBinding(binding);
+				.with("*.orange.*");
+		this.amqpAdmin.declareBinding(binding1);
+
+		final Binding binding2 = BindingBuilder
+				.bind(queue2)
+				.to(exchange)
+				.with("*.*.rabbit");
+		this.amqpAdmin.declareBinding(binding2);
+
+		final Binding binding3 = BindingBuilder
+				.bind(queue2)
+				.to(exchange)
+				.with("lazy.#");
+		this.amqpAdmin.declareBinding(binding3);
 
 		// send
 		this.amqpTemplate.convertAndSend(
 				exchange.getName(),
-				queue.getName(),
-				"test");
-
+				"quick.orange.rabbit",
+				"test message 1");
+		this.amqpTemplate.convertAndSend(
+				exchange.getName(),
+				"lazy.orange.elephant",
+				"test message 2");
+		this.amqpTemplate.convertAndSend(
+				exchange.getName(),
+				"quick.orange.fox",
+				"test message 3");
+		this.amqpTemplate.convertAndSend(
+				exchange.getName(),
+				"lazy.brown.fox",
+				"test message 4");
+		this.amqpTemplate.convertAndSend(
+				exchange.getName(),
+				"lazy.pink.rabbit",
+				"test message 5");
+		this.amqpTemplate.convertAndSend(
+				exchange.getName(),
+				"quick.brown.fox",
+				"test message 6");
 	}
 }
